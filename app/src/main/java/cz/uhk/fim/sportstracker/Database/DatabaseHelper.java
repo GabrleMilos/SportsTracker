d7b6994980836ca751a6fb999a935f3f2dae4ee4
MilosG <gabrlemilos@gmail.com>
2018-05-02 15:39:02 +0200

    DatabaseHelper => interfaces implemented

--- app/src/main/java/cz/uhk/fim/sportstracker/Database/DatabaseHelper.java
+++ app/src/main/java/cz/uhk/fim/sportstracker/Database/DatabaseHelper.java
@@ -4,7 +4,13 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseHelper extends SQLiteOpenHelper {
+import java.util.Collection;
+
+import cz.uhk.fim.sportstracker.Models.Activity;
+import cz.uhk.fim.sportstracker.Models.Position;
+import cz.uhk.fim.sportstracker.Models.User;
+
+public class DatabaseHelper extends SQLiteOpenHelper implements ActivityHelperInterface, PositionHelperInterface, UserHelperInterface {
 
     public DatabaseHelper(Context context){
         super(context, "database",null,1);
@@ -24,4 +30,63 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
 
+    @Override
+    public Activity getActivity(int activityId) {
+        return null;
+    }
+
+    @Override
+    public boolean insertActivity(Activity activity) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteActivity(Activity activity) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteActivity(int activityId) {
+        return false;
+    }
+
+    @Override
+    public Collection<Position> getActivityPositions(int activityId) {
+        return null;
+    }
+
+    @Override
+    public Position getPosition(int positionId) {
+        return null;
+    }
+
+    @Override
+    public boolean insertPosition(Position position) {
+        return false;
+    }
+
+    @Override
+    public boolean deletePosition(int positionId) {
+        return false;
+    }
+
+    @Override
+    public boolean deletePosition(Position position) {
+        return false;
+    }
+
+    @Override
+    public User getUser(int userId) {
+        return null;
+    }
+
+    @Override
+    public boolean addUser(User user) {
+        return false;
+    }
+
+    @Override
+    public Collection<Activity> getUserActivities(int userId) {
+        return null;
+    }
 }
